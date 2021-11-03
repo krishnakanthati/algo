54dc24e53403af9d60939fd6c018716bb4349f8e
krishnakanthati <krishnakanthati@outlook.com>
2021-11-03 17:57:54 +0530

    merge sort

---
+++ mergesort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        # recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # merge
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] =  left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+test_arr = [9, 8, 5, 7, 6, 2, 1, 8]
+
+merge_sort(test_arr)
+print(test_arr)
