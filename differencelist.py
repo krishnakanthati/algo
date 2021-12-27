a28c8266973cf5233f6197ba38898b3285a217a3
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2021-12-27 13:20:28 +0530

    difference list

--- differencelist.py
+++ differencelist.py
@@ -1,4 +1,6 @@
-a = [10, 5, 20, 40]
+import time
+
+a = [10, 5, 20, 40, 50, -5, 60, 40, 23, 1]
 b = a.copy()
 n = len(a)
 
@@ -16,17 +18,17 @@ def difference_array(l, r, x):
             d.append(b[i])
         else:
             d.append(b[i]-b[i-1])
+    d.append(0)
     d[l] = d[l] + x
     d[r+1] = d[r+1] - x
     for i in range(n):
         if i == 0:
             b[i] = d[i]
-            res = d[i]
         else:
-            res += d[i]
-            b[i] = res
+            b[i] = b[i-1] + d[i]
     return b
 
 
-print(update(0, 1, 10))
-print(difference_array(0, 1, 10))
+print("Without DA", update(0, 10, 10))
+print()
+print("With DA", difference_array(0, 10, 10))
