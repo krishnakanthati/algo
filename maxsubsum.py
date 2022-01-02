e5f6852c1449641306091cd36bf8210f499a615d
Krishna Kant Hati <krishnakant08@users.noreply.github.com>
2022-01-02 07:46:00 +0530

    maximum subarray sum brute force

--- maxsubsum.py
+++ maxsubsum.py
@@ -1,13 +1,25 @@
 # Maximum subarray sum brute force O(N^3)
 
-a = [14, -11, 1, 12]
+# a = [14, -11, 1, 12]
+# max = 0
+# for i in range(len(a)):
+#     for j in range(i, len(a)):
+#         sum = 0
+#         for k in range(i, j+1):
+#             sum += a[k]
+#         if sum > max:
+#             max = sum
+
+# print(max)  # 16
+
+# Maximum subarray sum brute force O(N^2)
+
+a = [14, 11, -1, -12]
 max = 0
 for i in range(len(a)):
+    sum = 0
     for j in range(i, len(a)):
-        sum = 0
-        for k in range(i, j+1):
-            sum += a[k]
+        sum += a[j]
         if sum > max:
             max = sum
-
-print(max)  # 16
+print(max)
