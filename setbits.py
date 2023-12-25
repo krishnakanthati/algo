c4a6fe6da957704f9f18e453f54072113a72cab7
Krishna Kant Hati <krishnakanthati@outlook.com>
2023-12-25 13:16:32 +0530

    setbits

---
+++ setbits.py
@@ -0,0 +1,36 @@
+# traditional way
+# 107 1101011
+
+n = 107
+
+c = 0
+while n != 0:
+    if n % 2 != 0:  # check if the number is odd
+        c += 1
+    n //= 2
+
+# print(c)  # 5
+
+
+# O(MSB)
+n = 107
+c = 0
+
+while n != 0:
+    if n & 1 == 1:  # check if the number is odd
+        c += 1
+    n >>= 1
+
+# print(c)  # 5
+
+
+# O(Set Bit)
+n = 107
+c = 0
+
+while n != 0:
+    n &= n-1  # count the last set bit
+    c += 1
+    n >>= 1
+
+print(c)  # 5
--- subsets.py
+++ subsets.py
@@ -8,8 +8,21 @@ l = []
 for num in range(2 ** len(a)):
     ds = []
     for bit in range(0, len(a)):
-        if num & 1 << bit:
-            ds.append(a[bit])
+        if num & 1 << bit:  # checks if the bit is set
+            ds.append(a[bit])  # if the bit is set then appends the value to ds
     l.append(ds)
 
 print(l)  # [[], [2], [3], [2, 3], [4], [2, 4], [3, 4], [2, 3, 4]]
+
+
+"""
+a: * 2 3 4
+0: 0 0 0 1
+1: 0 0 1 0
+2: 0 0 1 1
+3: 0 1 0 0
+4: 0 1 0 1
+5: 0 1 1 0
+6: 0 1 1 1
+7: 1 0 0 0
+"""
